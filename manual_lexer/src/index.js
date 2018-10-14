74d9ac93c939b6fdca8f953c772613cd65999e89
Aleksey Rysaev <techpriest2007@yandex.ru>
2018-10-14 13:16:47 +0300

    Removed commented code

--- manual_lexer/src/index.js
+++ manual_lexer/src/index.js
@@ -2,26 +2,6 @@ const Letterer = require('./Letterer');
 const Worder = require('./Worder');
 const Tokenizer = require('./Tokenizer');
 
-/*const {PassThrough} = require('stream');
-
-class Lexer extends PassThrough {
-    constructor(opts = {}) {
-        super(Object.assign(opts, {readableObjectMode: true}));
-        this.transformStream = null;
-        this.on('pipe', function(source) {
-            source.unpipe(this);
-            this.transformStream = source
-                .pipe(new Letterer())
-                .pipe(new Worder())
-                .pipe(new Tokenizer());
-        }.bind(this));
-    }
-
-    pipe (destination, options) {
-        return this.transformStream.pipe(destination, options);
-    };
-}*/
-
 /**
  * @param {Readable} source 
  * @param {Writable} destination 
